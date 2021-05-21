877af094c00967050cb7e73350e179951684c9b6
ilyaalekseenko <78536213+ilyaalekseenko@users.noreply.github.com>
2021-05-21 14:23:40 +0300

    Add words count

--- projectDjango1/views.py
+++ projectDjango1/views.py
@@ -10,4 +10,5 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext':reversed_text})
+    # len_text = len(user_text.split())
+    return render(request, 'reverse.html', {'usertext':user_text, 'reversedtext':reversed_text, 'lentext':len(user_text.split())})
